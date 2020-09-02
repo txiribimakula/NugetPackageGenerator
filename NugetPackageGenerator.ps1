1cadfd0421526f0d7d7941c85c077fae9703a7e1
Pablo Tobalina <pablotobalina@gmail.com>
2020-09-02 17:32:15 +0200

    Add path configuration.

--- NugetPackageGenerator.ps1
+++ NugetPackageGenerator.ps1
@@ -1,11 +1,23 @@
 param (
-    [switch]$debug
+    [switch]$debug,
+    [switch]$local
 )
 
 Import-Module -Name ".\NugetPackageGenerator.psm1"
+
+$localPath = "C://localPath"
+$serverPath = "//serverPath"
 
 $files = 
     "fileName1",
     "fileName2"
 
+$path
+if($local) {
+    $path = $localPath
+} else {
+    $path = $serverPath
+}
+
 CopyFiles $files $path $debug
+
--- NugetPackageGenerator.psm1
+++ NugetPackageGenerator.psm1
@@ -1,5 +1,5 @@
 function CopyFiles([string[]]$files, [string]$path, [bool]$isDebug) {
     foreach ($file in $files) {
-        Write-Host "Copy" $file
+        Write-Host "Copy" $path"/"$file
     }
 }
