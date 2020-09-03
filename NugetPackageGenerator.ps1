6ce31bfda9bb1d2d38a545e5a8a948dbbb368070
Pablo Tobalina <pablotobalina@gmail.com>
2020-09-03 17:52:40 +0200

    Remove blank lines.

--- NugetPackageGenerator.ps1
+++ NugetPackageGenerator.ps1
@@ -6,28 +6,22 @@ param (
     [int]$patchV,
     [string]$notes
 )
-
 Import-Module -Name ".\NugetPackageGenerator.psm1"
-
 $nuspec = @{
     Path = ".\example.nuspec";
     Ns = "http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd";
 }
-
 $pathOptions = @{
     Local = "C://localPath";
     Server = "//serverPath";
 }
-
 $files = 
     "fileName1",
     "fileName2"
  
-
 $inputData = GetInputData $local $pathOptions $debug $nuspec $majorV $minorV $patchV
 
 CopyFiles $inputData.Path $files $inputData.Extension
 
 SetVersion $nuspec $inputData.Version
-
 SetReleaseNotes $nuspec $inputData.ReleaseNotes
